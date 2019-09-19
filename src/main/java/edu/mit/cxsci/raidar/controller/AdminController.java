65d4bd3e89c26464b799b257d4634999d9443804
mizanul <mizanul.chowdhury@gmail.com>
2019-09-18 22:01:42 -0400

    Update AdminController.java

--- src/main/java/edu/mit/cxsci/raidar/controller/AdminController.java
+++ src/main/java/edu/mit/cxsci/raidar/controller/AdminController.java
@@ -28,6 +28,12 @@ package edu.mit.cxsci.raidar.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ *
+ * @author Mizanul H. Chowdhuury, MIT
+ * @version 1.0
+ **/
+
 @Controller
 public class AdminController {
 	 @RequestMapping("/admin")
